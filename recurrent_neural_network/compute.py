23fce72b463afac577b4bfc196577a0a198be37a
vebovs <vebovs@hotmail.no>
2019-09-14 17:18:05 +0200

    added a low level hello world model and a high level word predictior

--- recurrent_neural_network/compute.py
+++ recurrent_neural_network/compute.py
@@ -2,34 +2,32 @@ import numpy as np
 import tensorflow as tf
 import emoji
 
-batch_size = 1
+batch_size = 100
 
-#char_encodings = [
-#    [1, 0, 0, 0, 0, 0, 0, 0],  # ' '
-#    [0, 1, 0, 0, 0, 0, 0, 0],  # 'h'
-#    [0, 0, 1, 0, 0, 0, 0, 0],  # 'e'
-#    [0, 0, 0, 1, 0, 0, 0, 0],  # 'l'
-#    [0, 0, 0, 0, 1, 0, 0, 0],  # 'o'
-#    [0, 0, 0, 0, 0, 1, 0, 0],  # 'w'
-#    [0, 0, 0, 0, 0, 0, 1, 0],  # 'r'
-#    [0, 0, 0, 0, 0, 0, 0, 1]   # 'd'
-#]
 char_encodings = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
+    [1, 0, 0, 0, 0, 0], #c
+    [0, 1, 0, 0, 0, 0], #a
+    [0, 0, 1, 0, 0, 0], #t
+    [0, 0, 0, 1, 0, 0], #r
+    [0, 0, 0, 0, 1, 0], #s
+    [0, 0, 0, 0, 0, 1]  #h
 ]
 encoding_size = np.shape(char_encodings)[1]
 
-#index_to_char = [' ', 'h', 'e', 'l', 'o', 'w', 'r', 'd']
-index_to_char = [' ', 'c', 'a', 't']
+index_to_char = ['c', 'a', 't', 'r', 's', 'h']
+categories = ['cat', 'rat', 'hat', '', '', '']
+index_categories = [
+    [1, 0, 0, 0, 0, 0], #cat
+    [0, 1, 0, 0, 0, 0], #rat
+    [0, 0, 1, 0, 0, 0], #hat
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
 char_to_index = dict((char, i) for i, char in enumerate(index_to_char))
 
-#x_train = [[[char_encodings[0], char_encodings[1], char_encodings[2], char_encodings[3], char_encodings[3], char_encodings[4], char_encodings[0], char_encodings[5], char_encodings[4], char_encodings[6], char_encodings[3], char_encodings[7]]]]  # ' hello world'
-#y_train = [[[char_encodings[1], char_encodings[2], char_encodings[3], char_encodings[3], char_encodings[4], char_encodings[0], char_encodings[5], char_encodings[4], char_encodings[6], char_encodings[3], char_encodings[7], char_encodings[0]]]]  # 'hello world '
-x_train = [[[char_encodings[0], char_encodings[1], char_encodings[2], char_encodings[3]]]]
-y_train = [[[char_encodings[1], char_encodings[2], char_encodings[3], char_encodings[0]]]]
+x_train = [[[char_encodings[0], char_encodings[1], char_encodings[2]], [char_encodings[3], char_encodings[1], char_encodings[2]], [char_encodings[5], char_encodings[1], char_encodings[2]]]] #cat, rat, hat
+y_train = [[[index_categories[0]], [index_categories[1]], [index_categories[2]]]]
 
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.LSTM(128, input_shape=(None, encoding_size), return_sequences=True))
@@ -44,47 +42,15 @@ def on_epoch_end(epoch, data):
         print("epoch", epoch)
         print("loss", data['loss'])
 
-        # Generate text from the initial text
-        text = ' c'
+        #Test word
+        text = 'rt'
         for i in range(50):
-            x = np.zeros((1, i + 2, encoding_size))
+            x = np.zeros((1, i + len(text) + 1, encoding_size))
             for t, char in enumerate(text):
                 x[0, t, char_to_index[char]] = 1
             y = model.predict(x)[0][-1]
-            text += index_to_char[y.argmax()]
-        print(text.strip())
-        print(emoji.emojize(':' + text.strip() + ':'))
+            text = categories[y.argmax()]
+        print(emoji.emojize(':' + text + ':'))
 
 
 model.fit(x_train, y_train, batch_size=batch_size, epochs=500, verbose=False, callbacks=[tf.keras.callbacks.LambdaCallback(on_epoch_end=on_epoch_end)])
-
-char_encodings = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-]
-encoding_size = np.shape(char_encodings)[1]
-index_to_char = [' ', 'b', 'a', 't']
-char_to_index = dict((char, i) for i, char in enumerate(index_to_char))
-
-x_test = [[[char_encodings[0], char_encodings[1], char_encodings[2], char_encodings[3]]]]
-
-prediction = model.predict(x_test, batch_size=batch_size, verbose=0, steps=None, callbacks=None)
-
-result = [[[0], [0], [0], [0]]]
-for i in range(len(prediction)):
-    for j in range(len(prediction[i])):
-        result[i][j][0] = np.where(prediction[i][j] == np.amax(prediction[i][j]))
-
-values = []
-for i in range(len(index_to_char)):
-    values.append(result[0][i][0][0][0])
-
-word = []
-for i in range(len(index_to_char)):
-    word.append(index_to_char[values[i]])
-
-word = ''.join(word)
-print(word.strip())
-print(emoji.emojize(':' + word.strip() + ':'))
--- recurrent_neural_network/generate.py
+++ recurrent_neural_network/generate.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 
-
 class LongShortTermMemoryModel:
     def __init__(self, encoding_size):
         # Model constants
@@ -34,18 +33,18 @@ class LongShortTermMemoryModel:
 
 
 char_encodings = [
-    [1, 0, 0, 0, 0, 0, 0, 0],  # ' '
-    [0, 1, 0, 0, 0, 0, 0, 0],  # 'h'
-    [0, 0, 1, 0, 0, 0, 0, 0],  # 'e'
-    [0, 0, 0, 1, 0, 0, 0, 0],  # 'l'
-    [0, 0, 0, 0, 1, 0, 0, 0],  # 'o'
-    [0, 0, 0, 0, 0, 1, 0, 0],  # 'w'
-    [0, 0, 0, 0, 0, 0, 1, 0],  # 'r'
-    [0, 0, 0, 0, 0, 0, 0, 1]   # 'd'
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # ' '
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 'h'
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],  # 'e'
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],  # 'l'
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],  # 'o'
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],  # 'w'
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],  # 'r'
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0 ,0, 0],  # 'd'
 ]
 encoding_size = np.shape(char_encodings)[1]
 
-index_to_char = [' ', 'h', 'e', 'l', 'o', 'w', 'r', 'd']
+index_to_char = [' ', 'h', 'e', 'l', 'o', 'w', 'r', 'd', 'c', 'a', 't']
 
 x_train = [char_encodings[0], char_encodings[1], char_encodings[2], char_encodings[3], char_encodings[3], char_encodings[4], char_encodings[0], char_encodings[5], char_encodings[4], char_encodings[6], char_encodings[3], char_encodings[7]]  # ' hello world'
 y_train = [char_encodings[1], char_encodings[2], char_encodings[3], char_encodings[3], char_encodings[4], char_encodings[0], char_encodings[5], char_encodings[4], char_encodings[6], char_encodings[3], char_encodings[7], char_encodings[0]]  # 'hello world '
@@ -71,7 +70,7 @@ for epoch in range(500):
         print("epoch", epoch)
         print("loss", session.run(model.loss, {model.batch_size: 1, model.x: [x_train], model.y: [y_train], model.in_state: zero_state}))
 
-        # Generate characters from the initial characters ' h'
+        # Generate characters from the initial characters
         state = session.run(model.in_state, {model.batch_size: 1})
         text = ' h'
         y, state = session.run([model.f, model.out_state], {model.batch_size: 1, model.x: [[char_encodings[0]]], model.in_state: state})  # ' '
